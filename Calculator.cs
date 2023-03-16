9cf091a487ad08e7b5d4caf6f25da8b1eb4b1671
elingfalah14 <elingfalah11@students.amikom.ac.id>
2023-03-16 14:24:34 +0700

    menambah file Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+class calculator
+{
+	static void main(string[] args)
+	{int a = 10;
+	int b = 6;
+	
+	Console.WriteLane("hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b));
+	Console.WriteLane("hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a,b));
+	
+	Console.WriteLane("\nTekan sembarang key untuk keluar");
+	Console.ReadKey;
+	}
+	
+	static int Penambahan(int a. int B)
+	{
+		return a+b;
+	}
+	static int pengurangan (int a, int b)
+	{
+		return a-b;
+	}
